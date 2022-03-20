12b0f14ca57f127547e88e6c632acbe6718f8876
ZakarieA <36860288+zak-al@users.noreply.github.com>
2022-03-20 20:01:04 +0100

    Résolution de quelques bugs...

--- voting.c
+++ voting.c
@@ -5,7 +5,7 @@
 #include "voting.h"
 
 Signature* init_signature(long* content, size_t size) {
-    Signature* signature = malloc(sizeof(signature));
+    Signature* signature = malloc(sizeof(Signature));
     if (!signature) {
         fprintf(stderr, "[init_signature] : erreur lors de l'allocation de la signature :(");
         return NULL;
@@ -44,19 +44,18 @@ char* signature_to_str(Signature* sgn) {
 
 
 Signature* str_to_siganture(char* str) {
-    int len = strlen(str);
+    size_t len = strlen(str);
     long *content = (long *) malloc(sizeof(long) * len);
     int num = 0;
     char buffer[256];
     int pos = 0;
-    for (int i = 0; i < len; i++) {
-        if (str[i] != "#") {
+    for (size_t i = 0; i < len; i++) {
+        if (str[i] != '#') {
             buffer[pos] = str[i];
             pos = pos + 1;
         } else {
             if (pos != 0) {
                 buffer[pos] = '\0';
-
                 sscanf(buffer, "%lx", &(content[num]));
                 num = num + 1;
                 pos = 0;
