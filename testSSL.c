1e9aca29e3bd7244bc7ee482afa5abead509a4a8
Zhico <huazc1002@outlook.com>
2022-04-13 14:16:50 +0200

    Màj le 13/4

---
+++ testSSL.c
@@ -0,0 +1,19 @@
+//
+// Created by Zhichun Hua on 13/04/2022.
+//
+
+#include <openssl/sha.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    const char *s = "Rosetta code";
+    unsigned char *d = (unsigned char*) SHA256( (const unsigned char*) s, strlen(s),0);
+    int i;
+    for (i = 0; i < SHA256_DIGEST_LENGTH; i++){
+        printf("%02x",d[i]);
+    }
+    putchar('\n');
+
+    return 0;
+}
