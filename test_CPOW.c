cab39c33cdd0dfc2356cd1586eddf2c66029fe49
Zhico <huazc1002@outlook.com>
2022-05-08 23:33:55 +0200

    test CPOW

--- test_CPOW.c
+++ test_CPOW.c
@@ -2,29 +2,29 @@
 #include "blockchain.h"
 #include <time.h>
 
+#define NB_TEST 5
+
 int main(){
-    long p = random_prime_number(7, 8, 5000);
-    long q;
-    do{
-        q = random_prime_number(7, 8, 5000);
-    }while(p == q);     //pour assurer p et q distincts
+    generate_random_data(10,3);
+    CellProtected * votes = read_protected();
+    CellKey* voters = read_public_keys("keys.txt");
+    printf("voters initialised\n");
+    Key* author = voters->data;
+    Key* author1 = voters->next->next;
 
-    //la creation des cles
-    long n, s, u;
-    generate_key_values(p, q, &n, &s, &u);
-    printf("cle publique = (%ld, %ld) \n", s, n);
-    printf("cle privee = (%ld, %ld) \n", u, n);
+    Block *b1 = malloc(sizeof(Block));
+    b1->author = author1;
+    b1->previous_hash = NULL;
+    b1->votes = votes;
+    b1->nonce = 0;
+    b1->hash = (unsigned char *) strToHash(blockToStr(b1));
 
-    Key* blockKeyP = malloc(sizeof(Key));
-    Key* blockKeyS = malloc(sizeof(Key));
-    init_key(blockKeyP, s, n);
-    init_key(blockKeyP, u, n);
-
-    Block* b = malloc(sizeof(Block));
-    b -> author = blockKeyP;
-    b -> previous_hash = NULL;
-    b -> nonce = 0;
-    b -> hash = (unsigned char*)strToHash(blockToStr(b));
+    Block *b = malloc(sizeof(Block));
+    b->author = author;
+    b->previous_hash = strdup(b1->hash);
+    b->votes = votes;
+    b->nonce = 0;
+    b->hash = (unsigned char *) strToHash(blockToStr(b));
 
     //Question 1.5
     //pour comparer les performances des deux methodes
@@ -32,26 +32,25 @@ int main(){
 
     clock_t temps_init, temps_fin;
     double temps_cpu;
-    FILE *fic = fopen("sortieTime_CPOW.txt", "w");  //stocker les donnees dans le ficher
-    if(fic == NULL){
+    FILE * fic = fopen("sortieTime_CPOW.txt", "w");  //stocker les donnees dans le ficher
+    if (fic == NULL) {
         printf("Erreur de fichier\n");
         return 0;
     }
     int i = 1;
-    while(i < 4){
+    while (i < 4) {
         temps_init = clock();
         compute_proof_of_work(b, i);
         temps_fin = clock();
-        temps_cpu = ((double)(temps_fin - temps_init)/CLOCKS_PER_SEC);
+        temps_cpu = ((double) (temps_fin - temps_init) / CLOCKS_PER_SEC);
         fprintf(fic, "%d %f\n", i, temps_cpu);
-        if(temps_cpu >= 1)
+        if (temps_cpu >= 1)
             printf("%d\n", i);
         i++;
     }
     fclose(fic);
 
     freeBlock(b);
-    free(blockKeyS);
-
+    free(author);
     return 0;
 }
